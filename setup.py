55735a7b48921e50e313da637c7aa4e3d47669e7
Eliot <saltycrane@gmail.com>
2012-04-04 12:47:26 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Flask-EncryptedSession',
+    version='0.1.0',
+    url='http://github.com/saltycrane/flask-encryptedsession/',
+    license='BSD',
+    author='Eliot',
+    author_email='saltycrane@gmail.com',
+    description='Flask encrypted cookie session',
+    packages=find_packages(),
+    platforms='any',
+    install_requires=[
+        'Flask>=0.8',
+        'pyasn1>=0.1.2',
+        'PyCrypto>=2.5',
+        'python-keyczar>=0.71b',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
